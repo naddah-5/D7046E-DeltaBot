9cbb117fa4304489eac945128378f4eb06d938d9
JeremyAugereau <augereau.jeremy@protonmail.com>
2023-02-24 10:28:38 +0100

    parser + save done

---
+++ src/data/DeltaCsvParser.py
@@ -0,0 +1,59 @@
+import csv
+import nltk
+import re
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+
+class DeltaCsvParser:
+    def __init__(self, csv_path):
+        self.stemmer = PorterStemmer()
+        self.data = []
+        with open(csv_path, 'r', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            i = 0
+            for row in reader:
+                
+                # for examples
+                if(i > 30) :
+                    break
+                i = i+1
+                
+
+                data_row = {"Text": row["Text"], "HelpfulnessNumerator": row["HelpfulnessNumerator"], "HelpfulnessDenominator": row["HelpfulnessDenominator"]}
+
+                data_row["Text"] = self.preProcesing(data_row["Text"])
+                helpfullnessscrore = int(data_row['HelpfulnessNumerator']) - int(data_row['HelpfulnessDenominator'])
+                
+                self.data.append((data_row['Text'],helpfullnessscrore))
+            print(self.data)
+
+
+    def preProcesing(self, review):
+        # Cleaning special character from the review
+        review = re.sub(pattern='[^a-zA-Z]', repl=' ', string=review)
+
+        # Converting the entire review into lower case
+        review = review.lower()
+
+        # Tokenizing the review by words
+        words = review.split()
+
+        # Removing the stop words
+        filtered_words = [word for word in words if word not in set(stopwords.words('english'))]
+
+        # Lemmatizing the words
+        lemmatized_words = [self.stemmer.stem(word) for word in filtered_words]
+
+        return lemmatized_words
+
+    def save(self, output_path):
+        with open(output_path, 'w', newline='', encoding='utf-8') as f:
+            writer = csv.DictWriter(f, fieldnames=["Text", "HelpfulnessScore"])
+            writer.writeheader()
+            for text,helpfullness in self.data:
+                writer.writerow({'Text':text,'HelpfulnessScore':helpfullness})
+            
---
+++ src/data/data.py
@@ -0,0 +1,11 @@
+from DeltaCsvParser import DeltaCsvParser
+import pandas as pd
+import seaborn as sns
+from fast_ml.model_development import train_valid_test_split
+
+if __name__ == "__main__":
+
+    #parser = DeltaCsvParser('src/data/dataset/Reviews.csv')
+    #parser.save('src/data/dataset/Precessed_Data.csv')
+
+    
