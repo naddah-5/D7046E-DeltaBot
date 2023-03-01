6bf22ea44b1cbc02eae17ca69ab2d3007385b592
JeremyAugereau <augereau.jeremy@protonmail.com>
2023-03-01 13:57:17 +0100

    DeltaData now returns Dataloaders

--- src/data/data.py
+++ src/data/data.py
@@ -6,7 +6,7 @@ from torch.utils.data import DataLoader
 
 class DeltaData():
     # set a path if you want to create a new dataset
-    def __init__(self,embedder, embedding_size : int, csv_source_path="",csv_proceed_path="",train_proportion =0.8,val_proportion=0.1,test_proportion=0.1):
+    def __init__(self,embedder, embedding_size : int, csv_source_path :  str = "",csv_proceed_path : str ="",train_proportion : float =0.8,val_proportion : float =0.1,test_proportion : float=0.1):
         
         if (train_proportion+val_proportion+test_proportion) != 1:
             raise ValueError("Sum of proportion should be equal to 1")
@@ -29,14 +29,14 @@ class DeltaData():
         self.test_size = len(self.dataset) -  self.train_size - self.val_size
         self.train,self.val,self.test = torch.utils.data.random_split(self.dataset, [self.train_size, self.val_size,self.test_size])
 
-    def getTrainingSet(self) -> DeltaDataset:
-        return self.train
+    def getTrainingLoader(self,batch_size : int = 10 ,shuffle : bool = True) -> DataLoader:
+        return torch.utils.data.DataLoader(self.train, batch_size=batch_size, shuffle=shuffle)
 
-    def getValidationSet(self)-> DeltaDataset:
-        return self.val
+    def getValidationLoader(self,batch_size : int = 10 ,shuffle : bool = True)-> DataLoader:
+        return torch.utils.data.DataLoader(self.val, batch_size=batch_size, shuffle=shuffle)
 
-    def getTestingSet(self)-> DeltaDataset:
-        return self.test
+    def getTestingLoader(self,batch_size : int = 10 ,shuffle : bool = True)-> DataLoader:
+        return torch.utils.data.DataLoader(self.test, batch_size=batch_size, shuffle=shuffle)
 
 """
 # How to use a DeltaData object (example)
