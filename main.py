0207247af046e1f35692ffb2805be4a62a1f0ade
JeremyAugereau <augereau.jeremy@protonmail.com>
2023-03-03 15:23:20 +0100

    merge accuracy plot and loss plot

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     DATASET_PATH='src/data/dataset/Reviews.csv'
     neural_model = NeuralModel()
     trainer = Train(epochs=10)
-    data = DeltaData(embedder=DeltaEmbedder(), embedding_size=300,csv_proceed_path="src/data/dataset/little_dataset.csv")
+    data = DeltaData(embedder=DeltaEmbedder(), embedding_size=300,csv_proceed_path="src/data/dataset/Processed_Data.csv")
 
     trainer.run_training(dataset=data, network=neural_model.network)
 
