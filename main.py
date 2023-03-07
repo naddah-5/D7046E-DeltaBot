b37c827a91cb5a0b4a2bcc4caa0f23e4c08a6a7a
Lucke0011 <lukas.ledin@gmail.com>
2023-03-07 15:51:33 +0100

    client

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 """
 Main file, the orchestrator.
 """
+import torch
+from torch import nn
+
+from src.client.client import Client
 from src.model.neural_model import NeuralModel
 from src.train.trainer import Train
 from src.data.data import DeltaData
 from src.data.delta_embedder import DeltaEmbedder
+from src.data.delta_csv_parser import DeltaCsvParser
+
 
 def main():
     """
     Orchestrator for running our training.
     """
-    DATASET_PATH='src/data/dataset/Reviews.csv'
+    DATASET_PATH = 'src/data/dataset/Reviews.csv'
     neural_model = NeuralModel()
     trainer = Train(epochs=10, batch_size=10)
-    data = DeltaData(embedder=DeltaEmbedder(), embedding_size=300,csv_proceed_path="src/data/dataset/Processed_Data_test_medium.csv")
+    data = DeltaData(embedder=DeltaEmbedder(), embedding_size=300,
+                     csv_proceed_path="src/data/dataset/Processed_Data_test_medium.csv")
 
     trainer.run_training(dataset=data, network=neural_model.network, learning_rate=0.01)
 
 
-if __name__=='__main__':
-    main()
+if __name__ == '__main__':
+    # main()
+
+    model = NeuralModel()
+    model.network.load_state_dict(torch.load('src/model/model.pt'))
+    client = Client(model.network)
+    client.run()
---
+++ src/client/client.py
@@ -0,0 +1,60 @@
+import re
+
+from torch import nn
+import torch
+
+from src.data import delta_csv_parser
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+
+# nltk.download('stopwords')
+from src.data.delta_embedder import DeltaEmbedder
+
+
+def switch(predicted):
+    pass
+
+
+class Client():
+
+    def __init__(self, model: nn.Sequential):
+        self.model = model
+        self.stemmer = PorterStemmer()
+
+    def run(self):
+        review = input('Enter review: ')
+        processed_review = self.pre_procesing(review)
+        delta_embedder = DeltaEmbedder()
+        embedded_review = delta_embedder(processed_review, embedding_length=300)
+        with torch.no_grad():
+
+            prediction = self.model(embedded_review)
+            predicted = prediction.argmax()
+
+            match predicted:
+                case 0:
+                    print(0)
+                case 1:
+                    print(1)
+                case 2:
+                    print(2)
+
+    def pre_procesing(self, review: str) -> list:
+        formattedString = re.sub('<.*?>', 'link', review)
+
+        # Cleaning special character from the review
+        review = re.sub(pattern='[^a-zA-Z]', repl=' ', string=formattedString)
+
+        # Converting the entire review into lower case
+        review = review.lower()
+
+        # Tokenizing the review by words
+        words = review.split()
+
+        # Removing the stop words
+        filtered_words = [word for word in words if word not in set(stopwords.words('english'))]
+
+        stemmered_word = [self.stemmer.stem(word) for word in filtered_words]
+
+        return stemmered_word
