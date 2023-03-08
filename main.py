bf914e93626630615ab1bee98b1016f4fbe5194e
Lucke0011 <lukas.ledin@gmail.com>
2023-03-08 20:10:44 +0100

    62% acc

--- main.py
+++ main.py
@@ -16,15 +16,15 @@ def main():
     """
     DATASET_PATH = 'src/data/dataset/Reviews.csv'
     neural_model = NeuralModel()
-    trainer = Train(epochs=10, batch_size=10)
+    trainer = Train(epochs=30, batch_size=10)
     data = DeltaData(embedder=DeltaEmbedder(),
-                     embedding_size=300, csv_proceed_path="src/data/dataset/medium_sampled_dataset.csv")
+                     embedding_size=300, csv_proceed_path="src/data/medium_sampled_dataset.csv")
 
     neural_model.network = trainer.run_training(dataset=data, network=neural_model.network, learning_rate=0.001)
     neural_model.save_network('network.pt')
 
 if __name__ == '__main__':
-    #main()
+    # main()
 
     model = NeuralModel()
     model.network.load_state_dict(torch.load('network.pt'))
